51bebcc577d087520cf3ce93e9883049a6514c12
Judioo <git@Judioo.com>
2012-11-05 11:20:24 +0000

    formatting

--- lib/GanymedeGenip/Cell.pm
+++ lib/GanymedeGenip/Cell.pm
@@ -4,15 +4,16 @@ use Moose;
   has value => (is => 'rw', isa => 'Str', default => sub{0});
   
   around 'value' => sub {
-  my $orig    = shift;
-  my $self    = shift;
-  
-  return $self->$orig unless defined $_[0];
-  # replace empty string with 0
-  # equivalent of sayin cell->value(0)
-  return $self->$orig(0) if($_[0] eq '');
-  $self->$orig(@_);
-};
+    my $orig    = shift;
+    my $self    = shift;
+    
+    return $self->$orig unless defined $_[0];
+    # replace empty string with 0
+    # equivalent of sayin cell->value(0)
+    return $self->$orig(0) if($_[0] eq '');
+    $self->$orig(@_);
+  };
+
 
 no Moose;
 __PACKAGE__->meta->make_immutable;
