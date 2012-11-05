1e9e176a99fc27876c88ceb89f6ca666eaf1acfe
Judioo <git@Judioo.com>
2012-11-05 12:45:02 +0000

    created row and cols indexes
    test still failing :)

--- lib/GanymedeGenip/Spreadsheet.pm
+++ lib/GanymedeGenip/Spreadsheet.pm
@@ -8,6 +8,11 @@ use constant DEFAULT_COLS_SIZE  => 10;
   has 'rows'    => (is => 'ro', isa => 'Int', default => DEFAULT_ROWS_SIZE());
   has 'columns' => (is => 'ro', isa => 'Int', default => DEFAULT_COLS_SIZE()); 
 
+  has 'validColumns'  
+                => ( is => 'rw', isa => 'HashRef[Str]', default => sub{{}});
+  has 'validRows'  
+                => ( is => 'rw', isa => 'HashRef[Str]', default => sub{{}});
+
 sub BUILD {
   my $self  = shift;
   
@@ -15,12 +20,24 @@ sub BUILD {
     die "Invalid $method not greater than 0"
       unless($self->$method > 0);
   }
+
+  # build grid validation indexes
+  $self->_create_indexes;
 }
 
 sub getCellValue {
   my $self    = shift;
   my $loc     = shift;
 
+}
+
+
+sub _create_indexes {
+  my $self    = shift;
+
+  my $char    = 'A';
+  $self->validColumns ( {map{ $char++ => 1 } (1..$self->columns)} );
+  $self->validRows    ( {map{ $_ => 1 } (1..$self->rows)} );
 }
 
 no Moose;
